0c5c4470f9d692619d29d65afb7c664e0fa2dc54
yichiehliu <jacvd7@gmail.com>
2020-05-28 16:39:58 +0800

    .

--- index.js
+++ index.js
@@ -28,11 +28,99 @@ function InternTitleToCom() {
 // console.log(projectTBB.position().left)
 
 
+function handleStepEnter({ element, direction }) {
+    if (direction == 'down') {
+        element.classList.add('animate');
+    }
+    // console.log(element, direction);
+}
+
+
+function handleStepExit({ element, direction }) {
+    if (direction == 'up') {
+        element.classList.remove('animate');
+    }
+    // console.log(element, direction);
+}
+
+var scroller_myimage = scrollama();
+scroller_myimage.setup({
+    step: ".myimage-container",
+    offset: .8,
+    debug: false
+})
+    .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit);
+
+var scroller_selfintro = scrollama();
+scroller_selfintro.setup({
+    step: ".self-intro",
+    offset: .8,
+    debug: false
+})
+    .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit);
+
+var scroller_title = scrollama();
+scroller_title.setup({
+    step: ".title",
+    offset: .8,
+    debug: false
+})
+    .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit);
+
 var scroller_intern = scrollama();
 scroller_intern.setup({
+    step: ".internships",
+    offset: .8,
+    debug: false
+})
+    .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit);
+var scroller_act_title = scrollama();
+scroller_act_title.setup({
+    step: ".act-title",
+    offset: .8,
+    debug: false
+})
+    .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit);
+
+var scroller_activity_1 = scrollama();
+scroller_activity_1.setup({
+    step: ".activity-tokyo",
+    offset: .7,
+    debug: false
+})
+    .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit);
+
+var scroller_activity_2 = scrollama();
+scroller_activity_2.setup({
+    step: ".activity-taipei",
+    offset: .7,
+    debug: false
+})
+    .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit);
+
+var scroller_activity_3 = scrollama();
+scroller_activity_3.setup({
+    step: ".activity-china",
+    offset: .7,
+    debug: false
+})
+    .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit);
+
+
+
+var scroller_intern_intro = scrollama();
+scroller_intern_intro.setup({
     step: ".intern-intro",
     offset: .16,
-    debug: true
+    debug: false
 })
     .onStepEnter(handleStepEnter)
     .onStepExit(handleStepExit);
@@ -41,34 +129,28 @@ scroller_intern.setup({
 var scroller_project = scrollama();
 scroller_project.setup({
     step: ".projects",
-    offset: .5,
-    debug: true
+    offset: .6,
+    debug: false
 })
     .onStepEnter(handleStepEnter)
     .onStepExit(handleStepExit);
 
 
-function handleStepEnter({ element, direction }) {
-    if (direction == 'down') {
-        element.classList.add('animate');
-    }
-    console.log(element, direction);
-}
-
-
-function handleStepExit({ element, direction }) {
-    if (direction == 'up') {
-        element.classList.remove('animate');
-    }
-    console.log(element, direction);
-}
+var scroller_contact = scrollama();
+scroller_contact.setup({
+    step: ".contact",
+    offset: .8,
+    debug: false
+})
+    .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit);
 
 
 // var scroller_2 = scrollama();
 // scroller_2.setup({
 //     step: ".intern-intro",
 //     offset: .21,
-//     debug: true
+//     debug: false
 // })
 //     .onStepExit(handleStepExit);
 
