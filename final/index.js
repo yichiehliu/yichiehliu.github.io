1fa6d638bce928356d993adcb3ee96c8d8dd2154
yichiehliu <jacvd7@gmail.com>
2020-06-21 04:23:52 +0800

    .

---
+++ final/index.js
@@ -0,0 +1,24 @@
+
+
+
+
+$(".Difficulty").on('change', function () {
+    var value = $(this).val();
+    // console.log(value)
+    if (value == "easy") {
+        $(".width-input").val(8);
+        $(".height-input").val(8);
+        $(".bomb-input").val(10);
+    }
+    else if (value == "medium") {
+        $(".width-input").val(16);
+        $(".height-input").val(16);
+        $(".bomb-input").val(40);
+    }
+    else if (value == "hard") {
+        $(".width-input").val(24);
+        $(".height-input").val(24);
+        $(".bomb-input").val(99);
+    }
+
+})
